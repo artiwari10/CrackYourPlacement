fadd41bea4af7002527e2eecfe8fef0677900f08
Anmol Ratan TIwari <66309398+artiwari10@users.noreply.github.com>
2024-07-20 18:43:21 +0530

    Add files via upload

---
+++ Day 6/Find Pair Given Difference.java
@@ -0,0 +1,15 @@
+class Solution {
+    public int findPair(int n, int x, int[] arr) {
+        Arrays.sort(arr);
+        int i = 0, j = 1;
+        while(i<n && j<n) {
+            int diff = arr[j]-arr[i];
+            if(diff==x && i!=j) return 1;
+            else if(diff < x) j++;
+            else i++;
+            
+        }
+        
+        return -1;
+    }
+}
